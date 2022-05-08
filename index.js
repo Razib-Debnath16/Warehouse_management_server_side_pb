ba5e18dafe2619289cb43b32e899a03bf3ef6d8a
Razib-Debnath16 <razib.debnath016@gmail.com>
2022-05-08 16:05:27 +0600

    adding delete

--- index.js
+++ index.js
@@ -55,25 +55,7 @@ async function run() {
         res.send(phones);
 
     })
-    app.put('/product/:id', async (req, res) => {
-        const id = req.params.id;
-        const user = req.body;
-        const filter = { _id: ObjectId(id) };
-        const options = { upsert: true };
-        const updateDoc = {
-            $set: {
-                quantity: user.quantity,
-                sold: user.sold,
-                name: user.name,
-                email: user.email,
-                description: user.description,
-                supplierName: user.supplierName,
-                price: user.price
-            },
-        };
-        const result = await phoneCollection.updateOne(filter, updateDoc, options);
-        res.send(result);
-    })
+
 
 
 }
